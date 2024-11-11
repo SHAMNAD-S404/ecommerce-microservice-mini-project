73496a7f0a8a4763b2be85c98a27cc80f602b109
SHAMNAD-S404 <shamnadsakeer306@gmail.com>
2024-11-11 18:53:05 +0530

    fixed issue with api gateway cors

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -1,17 +1,8 @@
 
   import express from 'express';
-  import {createProxyMiddleware} from "http-proxy-middleware";
-  import cors from 'cors'
+  import { createProxyMiddleware } from "http-proxy-middleware";
   const app = express ();
   const PORT = process.env.API_PORT || 4000;
-
-  app.use(express.json() );
-  app.use(cors( {origin:'http://localhost:3000' } ));
-
-  app.use( (req,res,next) => {
-    console.log("Incoming request to API Gateway:", req.method, req.url);
-    next();
-  })
 
   const paths = {
     auth    : "http://localhost:5001",
@@ -21,21 +12,10 @@
   };
 
 
-
-  app.use('/auth', createProxyMiddleware({
-    target: paths.auth,
-    changeOrigin: true,
-    pathRewrite: { '^/auth': '' },
-    onProxyReq: (proxyReq, req, res) => {
-       console.log("Forwarding request to auth-service:", req.method, req.url);
-    },
-    timeout: 60000, // Set a higher timeout to prevent request abortion
- }));
- 
-
-  app.use ('/product', createProxyMiddleware({target:paths.product , changeOrigin:true}));
-  app.use ('/order' , createProxyMiddleware({target:paths.order , changeOrigin:true}));
-  app.use ('/notification' , createProxyMiddleware({target:paths.notification, changeOrigin:true}));
+  app.use('/auth', createProxyMiddleware({target: paths.auth,changeOrigin: true}));
+  app.use('/product', createProxyMiddleware({target:paths.product , changeOrigin:true}));
+  app.use('/order' , createProxyMiddleware({target:paths.order , changeOrigin:true}));
+  app.use('/notification' , createProxyMiddleware({target:paths.notification, changeOrigin:true}));
 
  
   app.listen(PORT , ()=> {
